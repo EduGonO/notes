10bd6ba9024ad740b31fc86e2c5cbf421d751b39
edu gonzalez <320ego@gmail.com>
2023-05-31 16:27:12 +0200

    Update markdown.tsx

--- app/helpers/markdown.tsx
+++ app/helpers/markdown.tsx
@@ -80,10 +80,10 @@ const TagTokenizerExtension: marked.TokenizerExtension = {
   name: 'tag',
   level: 'inline',
 
-  start: (src: string) => src.match(\\B#\\w[\\w-]+)?.index || -1,
+  start: (src: string) => src.search(/\B#\w+/),
 
   tokenizer: (src: string) => {
-    const rule = \\B#\\w[\\w-]+;
+    const rule = /\B#\w+/;
     const match = rule.exec(src);
 
     if (match) {
