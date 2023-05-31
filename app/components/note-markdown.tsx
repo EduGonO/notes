2df9f6a81ac66c424ad02881c84c2c46b29cc525
edu gonzalez <320ego@gmail.com>
2023-05-31 19:05:28 +0200

    Update note-markdown.tsx

--- app/components/note-markdown.tsx
+++ app/components/note-markdown.tsx
@@ -52,7 +52,7 @@ const tokensToElements = (tokens: marked.Tokens.Generic[], options: MarkdownOpti
 
 const textTokenToElement = (token: marked.Tokens.Text, options: MarkdownOptions) => {
   if (token.tokens?.length) {
-    return <span style={{marginLeft: '-7px'}}>{tokensToElements(token.tokens, options)}</span>
+    return <span style={{marginLeft: '-10px'}}>{tokensToElements(token.tokens, options)}</span>
   } else {
     return <span dangerouslySetInnerHTML={{__html: token.text}} />
   }
