17547803a76134fc6ee8edde143a532ef8a0e1d3
edu gonzalez <320ego@gmail.com>
2023-06-01 02:53:09 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,26 +6,26 @@ function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
       <Head>
-        <title>The Beginning of Infinity</title>
+        <title>edu notes</title>
 
         <meta
           name="description"
-          content="Notes on David Deutsch's groundbreaking book. All failures are due to lack of knowledge."
+          content="notes and ideas on topics, by eduardo gonzalez"
         />
         <meta
           name="og:description"
-          content="Notes on David Deutsch's groundbreaking book. All failures are due to lack of knowledge."
+          content="notes and ideas on topics, by eduardo gonzalez"
         />
-        <meta name="og:title" content="The Beginning of Infinity" />
-        <meta name="og:url" content="https://thebeginningofinfinity.xyz" />
-        <meta name="apple-mobile-web-app-title" content="The Beginning of Infinity" />
+        <meta name="og:title" content="edu notes" />
+        <meta name="og:url" content="https://edunotes.vercel.app" />
+        <meta name="apple-mobile-web-app-title" content="edu notes" />
 
         <meta name="twitter:card" content="summary" />
         <meta
           name="twitter:image"
           content="https://thebeginningofinfinity.xyz/twitter-summary-card.png"
         />
-        <meta name="og:title" content="The Beginning of Infinity" />
+        <meta name="og:title" content="edu notes" />
         <meta
           name="og:image"
           content="https://thebeginningofinfinity.xyz/twitter-summary-card.png"
