81c1239ea9fb6d7c85b2dc2dd4b796a4607a73c8
edu gonzalez <46654892+EduGonO@users.noreply.github.com>
2023-09-16 14:10:54 +0200

    snippet: 2 lines → reference?
    ndp jala a la primera jajaja

--- server/helpers/notes.ts
+++ server/helpers/notes.ts
@@ -34,6 +34,7 @@ const readNote = async (name: string): Promise<Note> => {
   }
 }
 
+/*
 const markdownToSnippet = (markdown: string): string => {
   return markdown
     .replace(/^#.+/g, '')
@@ -41,7 +42,14 @@ const markdownToSnippet = (markdown: string): string => {
     .filter((l) => l.trim())
     .slice(0, 2)
     .join(' ')
+}*/
+
+const markdownToSnippet = (markdown: string, noteName: string): string => {
+  const paragraphs = markdown.split(/\n\s*\n/);
+  const foundParagraph = paragraphs.find((paragraph) => paragraph.includes(`[[${noteName}]]`));
+  return foundParagraph ? foundParagraph : "";
 }
+
 
 const noteToNotePreview = (note: Note): NotePreview => {
   return {
@@ -51,6 +59,30 @@ const noteToNotePreview = (note: Note): NotePreview => {
   }
 }
 
+const getHydratedNote = async (name: string): Promise<Note | null> => {
+  const allNotes = await getNotes();
+  const note = allNotes.find((n) => n.path === name);
+
+  if (!note) return null;
+
+  const linkedFromNotes = allNotes
+    .filter((n) => n !== note && n.path !== NOTE_INDEX_NAME)
+    .filter((n) => n.markdown.includes(`[[${name}]]`))
+    .map((otherNote) => {
+      return {
+        path: otherNote.path,
+        title: otherNote.title,
+        snippet: markdownToSnippet(otherNote.markdown, name),
+      };
+    });
+
+  return {
+    ...note,
+    linkedFromNotes,
+  }
+}
+
+/*
 export const getHydratedNote = async (name: string): Promise<Note | null> => {
   const allNotes = await getNotes()
   const note = allNotes.find((n) => n.path === name)
@@ -66,7 +98,7 @@ export const getHydratedNote = async (name: string): Promise<Note | null> => {
     ...note,
     linkedFromNotes,
   }
-}
+}*/
 
 export const getNote = async (name: string): Promise<Note | null> => {
   try {
