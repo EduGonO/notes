a3435abe9a8646299b9b7a738499cc1fac892611
edu gonzalez <46654892+EduGonO@users.noreply.github.com>
2023-10-25 19:21:39 +0200

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,6 @@ function MyApp({Component, pageProps}: AppProps) {
         <meta name="og:title" content="edu notes" />
         <meta name="og:url" content="https://edunotes.vercel.app" />
         <meta name="apple-mobile-web-app-title" content="edu notes" />
-        <link rel="icon" type="image/x-icon" href="./public/favicon.ico">
         <meta name="icon" type="image/x-icon" href="./public/favicon.ico">
 
         <meta name="twitter:card" content="summary" />
